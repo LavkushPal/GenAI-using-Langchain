8ed7d8a5e9c71b003c165b516f841922fa4de21c
LavkushPal <lavkushpal134@gmail.com>
2026-02-04 04:04:24 +0530

    today i covered splitters and retriever in RAG

---
+++ RAG/vector_stores/chroma_db.py
@@ -0,0 +1,44 @@
+from langchain.vectorstores import Chroma
+from langchain.schema import Document
+
+
+doc1 = Document(
+        page_content="Virat Kohli is one of the most successful and consistent batsmen in IPL history. Known for his aggressive batting style and fitness, he has led the Royal Challengers Bangalore in multiple seasons.",
+        metadata={"team": "Royal Challengers Bangalore"}
+    )
+doc2 = Document(
+        page_content="Rohit Sharma is the most successful captain in IPL history, leading Mumbai Indians to five titles. He's known for his calm demeanor and ability to play big innings under pressure.",
+        metadata={"team": "Mumbai Indians"}
+    )
+doc3 = Document(
+        page_content="MS Dhoni, famously known as Captain Cool, has led Chennai Super Kings to multiple IPL titles. His finishing skills, wicketkeeping, and leadership are legendary.",
+        metadata={"team": "Chennai Super Kings"}
+    )
+doc4 = Document(
+        page_content="Jasprit Bumrah is considered one of the best fast bowlers in T20 cricket. Playing for Mumbai Indians, he is known for his yorkers and death-over expertise.",
+        metadata={"team": "Mumbai Indians"}
+    )
+doc5 = Document(
+        page_content="Ravindra Jadeja is a dynamic all-rounder who contributes with both bat and ball. Representing Chennai Super Kings, his quick fielding and match-winning performances make him a key player.",
+        metadata={"team": "Chennai Super Kings"}
+    )
+
+
+vector_stores= Chroma(
+    embedding_functions=OpenAIEmbeddings(),
+    perssistent_directory='my_chroma_store',
+    collection_name='ipl team '
+)
+
+vector_stores.add_documents([doc1, doc2, doc3, doc4, doc5])
+
+vector_stores.get(include=['documents','embeddings','metadatas'])
+
+res_list= vector_stores.similarity_search(
+    query='which ipl team has one of most successful bowler and batter',
+    k=2
+)
+
+print(res_list)
+
+print(res_list[0])
